00e3cad7c0a65ef209757d0e71e7c09ef2e21e4a
MukriXYZ <50009694+MukriXYZ@users.noreply.github.com>
2019-05-01 16:39:09 +0800

    Added round selector and major improvement.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 var running = 0;
 var yourWins = 0;
 var botWins = 0;
+var roundsnum = 5;
+var roundsDone = 0;
+var tie = 0;
+
+$(".container").hide();
 
 $("#rock").click(function() {
+    if (running == 1) {
+        return;
+    }
+
     startPlaying("rock", getRPSBot());
 
     $("#user-fa").removeClass("fa-user");
@@ -10,9 +19,14 @@ $("#rock").click(function() {
     $("#user-fa").removeClass("fa-hand-scissors");
 
     $("#user-fa").addClass("fa-hand-rock");
+    $("#rock").addClass("select");
 });
 
 $("#paper").click(function() {
+    if (running == 1) {
+        return;
+    }
+    
     startPlaying("paper", getRPSBot());
 
     $("#user-fa").removeClass("fa-user");
@@ -20,9 +34,14 @@ $("#paper").click(function() {
     $("#user-fa").removeClass("fa-hand-scissors");
 
     $("#user-fa").addClass("fa-hand-paper");
+    $("#paper").addClass("select");
 });
 
 $("#scissors").click(function() {
+    if (running == 1) {
+        return;
+    }
+
     startPlaying("scissors", getRPSBot());
 
     $("#user-fa").removeClass("fa-user");
@@ -30,6 +49,13 @@ $("#scissors").click(function() {
     $("#user-fa").removeClass("fa-hand-paper");
 
     $("#user-fa").addClass("fa-hand-scissors");
+    $("#scissors").addClass("select");
+});
+
+$("#round-selector").click(function (){
+    var round = parseInt($("#best-of").val());
+
+    startGame(round);
 });
 
 $("#rock").hover(function() {
@@ -49,6 +75,19 @@ $("#scissors").hover(function() {
 }, function() {
     $("#selector-name").text("");
 });
+
+function startGame(round) {
+    if (round > 0) {
+        $(".round-selector").fadeOut();
+        $(".container").fadeIn();
+        roundsnum = round;
+
+        $("#round-title").text(roundsnum + " Round(s)");
+    }
+    else {
+        $("#error").text("Round must be 1 or more!");
+    }
+}
 
 function getRPSBot() {
     var randomNum = Math.round(Math.random() * (30 - 0) + 0);
@@ -71,6 +110,7 @@ function startPlaying(hand, bothand) {
     }
 
     running = 1;
+    roundsDone++;
     var counter = 4;
 
     $("#bot-fa").addClass("fa-question-circle");
@@ -107,53 +147,46 @@ function startPlaying(hand, bothand) {
             if (hand == "rock") {
                 if (bothand == "rock") {
                     // Tied
-                    $("#winner").text("Tied!");
+                    updateScore("tied");
                 }
                 else if (bothand == "paper") {
                     // Bot wins
-                    $("#winner").text("You lost!");
                     updateScore("bot");
                 }
                 else { // Scissors
                     // User wins
-                    $("#winner").text("You win!");
                     updateScore("user");
                 }
             }
             else if (hand == "paper") {
                 if (bothand == "rock") {
                     // User wins
-                    $("#winner").text("You win!");
                     updateScore("user");
                 }
                 else if (bothand == "paper") {
                     // Tied
-                    $("#winner").text("Tied!");
+                    updateScore("tied");
                 }
                 else { // Scissors
                     // Bot wins
-                    $("#winner").text("You lost!");
                     updateScore("bot");
                 }
             }
             else { //Scissors
                 if (bothand == "rock") {
                     // Bot wins
-                    $("#winner").text("You lost!");
                     updateScore("bot");
                 }
                 else if (bothand == "paper") {
                     // User wins
-                    $("#winner").text("You win!");
                     updateScore("user");
                 }
                 else { // Scissors
                     // Tied wins
-                    $("#winner").text("Tied!");
+                    updateScore("tied");
                 }
             }
 
-            running = 0;
             clearInterval(interval);
         }
 
@@ -164,11 +197,76 @@ function updateScore(winner) {
     if (winner == "bot") {
         botWins++;
         $("#counter").html('<i class="fas fa-arrow-right" style="color: red"></i>');
+        $("#winner").text("You lost!");
     }
-    else {
+    else if (winner == "user") {
         yourWins++;
         $("#counter").html('<i class="fas fa-arrow-left" style="color: green"></i>');
+        $("#winner").text("You win!");
+    }
+    else {
+        tie++;
+        $("#counter").html('<i class="fas fa-arrows-alt-h" style="color: green"></i>');
+        $("#winner").text("Tie!");
     }
 
-    $("#score").text(yourWins + " | " + botWins);
+    $("#rock").removeClass("select");
+    $("#paper").removeClass("select");
+    $("#scissors").removeClass("select");
+
+    $("#score").text(yourWins + " | " + tie + " | " + botWins);
+
+    if (roundsDone >= roundsnum) {
+        $("#round-show").text("The winner is....");
+        setTimeout(function() {
+            showWinner();
+        }, 3000);
+        return;
+    }
+    else {
+        $("#round-show").text("Round: " + (roundsDone + 1));
+        running = 0;
+    }
+}
+
+function showWinner() {
+    var wins;
+
+    if (botWins > yourWins) {
+        wins = "Bot";
+    }
+    else if (yourWins > botWins) {
+        wins = "You";
+    }
+    else {
+        wins = "Tied";
+    }
+
+    $("#show-winner").text("Winner: " + wins + "!");
+    $(".round-selector").fadeIn();
+
+    roundsDone = 0;
+    roundsnum = 5;
+    yourWins = 0;
+    botWins = 0;
+    running = 0;
+
+    $("#score").text(yourWins + " | " + tie + " | " + botWins);
+    $("#counter").empty();
+    $(".container").fadeOut();
+
+    $("#user-fa").removeClass("fa-user");
+    $("#user-fa").removeClass("fa-hand-paper");
+    $("#user-fa").removeClass("fa-hand-scissors");
+    $("#user-fa").removeClass("fa-hand-rock");
+    $("#user-fa").addClass("fa-user");
+
+    $("#bot-fa").removeClass("fa-user");
+    $("#bot-fa").removeClass("fa-hand-paper");
+    $("#bot-fa").removeClass("fa-hand-scissors");
+    $("#bot-fa").removeClass("fa-hand-rock");
+    $("#bot-fa").addClass("fa-question-circle");
+
+    $("#round-show").text("Round: 1");
+    $("#winner").text("");
 }
